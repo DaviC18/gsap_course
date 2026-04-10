7b7f03a3e57691032115f53f2428a93a040d6dd4
Davi Curty <davicurty850@gmail.com>
2026-04-10 19:37:28 -0300

    feat: starting the gsap course

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div className="flex-center ">
+      <div>App</div>
+    </div>
+  );
+};
+
+export default App;
